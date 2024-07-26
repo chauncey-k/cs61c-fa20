6b41c2ac8cc0d12df05929ef5ff27b43f977a2f2
chen <1073695360@qq.com>
2024-07-26 05:06:43 -0400

    complete lab02

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,15 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t temp = *reg >> 1;
+    uint16_t reg_0 = *reg & 1;
+    uint16_t reg_2 = (*reg & (1 << 2)) >> 2;
+    uint16_t reg_3 = (*reg & (1 << 3)) >> 3;
+    uint16_t reg_5 = (*reg & (1 << 5)) >> 5;
+    uint16_t fr_0 = reg_0 ^ reg_2;
+    uint16_t fr_1 = fr_0 ^ reg_3;
+    uint16_t fr_2 = fr_1 ^ reg_5;
+    *reg = temp & ~(1 << 15);
+    *reg |= (fr_2 << 15);
 }
 
