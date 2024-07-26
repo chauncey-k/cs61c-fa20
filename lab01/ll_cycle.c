f83223f1f681abc46c59dc621d9c4592f2963472
chen <1073695360@qq.com>
2024-07-26 00:38:24 -0400

    Initial commit

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,27 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node * tortoise;
+    node * hare;
+    tortoise = head;
+    hare = head;
+    while(hare != NULL && tortoise != NULL)
+    {
+        hare = hare->next;
+        if (hare == NULL)
+        {
+            break;
+        }
+        hare = hare->next;
+        if (hare == NULL)
+        {
+            break;
+        }
+        tortoise = tortoise->next;
+        if (hare == tortoise)
+        {
+            return 1;
+        }
+    }
     return 0;
 }
