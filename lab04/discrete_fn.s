47ca1f6678acda0a12acbfa6deb9c453fc0f4546
chen <1073695360@qq.com>
2024-08-05 00:38:55 -0400

    complete lab04

--- lab04/discrete_fn.s
+++ lab04/discrete_fn.s
@@ -77,7 +77,11 @@ main:
 # Think: why might having a1 be useful?
 f:
     # YOUR CODE GOES HERE!
-
+    addi t0, a0, 3
+    slli t0, t0, 2
+    add t1, a1, x0
+    add t0, t0, t1
+    lw a0, 0(t0)
     jr ra               # Always remember to jr ra after your function!
 
 print_int:
